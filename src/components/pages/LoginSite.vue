0b2c24a22f1cbab72f5481247a187b524f44037d
firoj <fkfiroj02@gmail.com>
2022-08-20 21:00:03 -0700

    firebase

--- src/components/pages/LoginSite.vue
+++ src/components/pages/LoginSite.vue
@@ -1,26 +1,100 @@
 <template>
- <div class="login-all">
-  <div class="login">
-
+ <div class="signup-all">
+  <div class="signup">
+    <div class="sign">
+      <h5>Login </h5>
+      <form @submit.prevent="Login">
+    
+        <input class="size" type="text" placeholder="Enter your email" v-model="email"><br>
+       
+        <input class="size" type="password" placeholder="password" v-model="password"><br>
+       
+        <input class="btn" type="submit" value="Submit">
+        
+        <p>Don't have an account? <router-link class="first" to="/signup">Signup</router-link></p>
+      </form>
+    </div>
+  
   </div>
-
- </div>
+</div>
 </template>
 
 <script>
-
+import firebase from "firebase";
 
 
 export default {
-  name: 'LoginSiteSite',
+ 
+  data(){
+     return{
+      email:'',
+      password:''
+     }
   
+  },
+  methods:{
+    Login(){
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        ()=>{
+          alert('login successfully');
+          this.$router.replace("home");
+        },
+        (err)=>{
+            alert(err.message);
+        }
+      );
+    }
+  }
+
+
 }
+
+
+
 </script>
 
-<style>
-.login-all{
+<style scoped>
+.signup-all{
   width: 100%;
   height: 555px;
-  background-color: rgb(5, 5, 5);
+   background-color: rgb(5, 5, 5);
+   padding-top: 76px;
+}
+.signup{
+  width: 500px;
+  height: 350px;
+background-color: rgb(141, 170, 90);
+  margin-left: 400px;
+ 
+  border-radius: 7px;
+  padding-top: 50px;
+
+}
+.btn{
+  background-color: darkslategrey;
+ width: 300px;
+ color: white;
+ font-size: 18px;
+margin-top: 10px;
+margin-bottom: 9px;
+}
+.sign h5{
+  color: white;
+}
+.size{
+  width: 300px;
+ margin: 5px;
+ 
+}
+.line{
+  width: 300px;
+  color: white;
+}
+.sign p{
+  color: white;
+  font-size: 19px;
+}
+.first{
+  color: crimson;
 }
 </style>
--- src/components/pages/SignupSite.vue
+++ src/components/pages/SignupSite.vue
@@ -2,11 +2,16 @@
 <div class="signup-all">
   <div class="signup">
     <div class="sign">
+      <h5>Sign Up </h5>
       <form @submit.prevent="Signup">
-        <input type="text" placeholder="email" v-model="email">
-        <input type="password" placeholder="password" v-model="password">
+    
+        <input class="size" type="text" placeholder="Enter your email" v-model="email"><br>
        
-        <input type="submit" value="Signup">
+        <input class="size" type="password" placeholder="password" v-model="password"><br>
+       
+        <input class="btn" type="submit" value="Submit">
+        
+        <p>Already have an account? <router-link class="first" to="/login">Login</router-link></p>
       </form>
     </div>
   
@@ -50,15 +55,44 @@ export default {
   width: 100%;
   height: 555px;
    background-color: rgb(5, 5, 5);
+   padding-top: 76px;
 }
 .signup{
   width: 500px;
-  height: 400px;
-  background-color: white;
-  margin-left: 100px;
+  height: 350px;
+background-color: rgb(141, 170, 90);
+  margin-left: 400px;
  
   border-radius: 7px;
+  padding-top: 50px;
 
+}
+.btn{
+  background-color: darkslategrey;
+ width: 300px;
+ color: white;
+ font-size: 18px;
+margin-top: 10px;
+margin-bottom: 9px;
+}
+.sign h5{
+  color: white;
+}
+.size{
+  width: 300px;
+ margin: 5px;
+ 
+}
+.line{
+  width: 300px;
+  color: white;
+}
+.sign p{
+  color: white;
+  font-size: 19px;
+}
+.first{
+  color: crimson;
 }
 
 </style>
